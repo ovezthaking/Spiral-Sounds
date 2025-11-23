aebc5dbfdd66393a2577d88358667fb079ffcb59
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-11-23 19:16:54 +0100

    feat: Recreate database schema and seed data for products and users

---
+++ sql/createUsersTable.js
@@ -0,0 +1,27 @@
+import sqlite3 from 'sqlite3'
+import { open } from 'sqlite'
+import path from 'node:path'
+
+async function createTable() {
+
+      const db = await open({
+            filename: path.join('database.db'),
+            driver: sqlite3.Database
+      }) 
+ 
+      await db.exec(`
+            CREATE TABLE users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            email TEXT UNIQUE NOT NULL,
+            username TEXT UNIQUE NOT NULL,
+            password TEXT NOT NULL,
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+            );
+      `)
+
+      await db.close()
+      console.log('table created')
+}
+
+createTable() 
