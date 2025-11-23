6dadf49b258ebc1604a6fb8339b79363ccefc764
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-11-23 19:49:56 +0100

    feat(auth): implement user registration logic with validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,25 @@
-import { registerUser } from "../controllers/authController.js";
-import express from 'express';
+import validator from 'validator'
 
-export const authRouter = express.Router();
+export async function registerUser(req, res) {
 
-authRouter.post('/register', registerUser)
+  let { name, email, username, password } = req.body
+
+  if (!name || !email || !username || !password){
+    return res.status(400).json({error: 'All the fields are required'});
+  }
+
+  name = name.trim()
+  email = email.trim()
+  username = username.trim()
+
+  if (!/^[a-zA-Z0-9_-]{1,20}$/.test(username)){
+    return res.status(400).json({error: 'username must be 1-20 characters, using letters, numbers, _ or -.'})
+  }
+
+  if (!validator.isEmail(email)){
+    return res.status(400).json({error: 'Bad format of email'})
+  }
+
+  console.log(req.body)
+
+}
