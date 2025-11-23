a7fba3273c77bac34c715e66fffc3eba2ae33e45
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-11-23 19:29:05 +0100

    feat(auth): implement user registration route and controller

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import { productsRouter } from './routes/products.js'
+import { authRouter } from './routes/auth.js'
 
 const app = express()
 const PORT = 8000
+
+app.use(express.json())
 
 
 app.use(express.static('public'))
@@ -10,6 +13,8 @@ app.use(express.static('public'))
 
 app.use('/api/products', productsRouter)
 
+
+app.use('/api/auth', authRouter)
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`)
