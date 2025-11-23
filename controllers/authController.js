595f308aa27f96352ac3296ba86ba7db7a811542
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-11-23 20:27:15 +0100

    feat(auth): enhance user registration with validation and database checks

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,59 @@
+import validator from 'validator'
+import { getDBConnection } from '../db/db.js';
+
+
 export async function registerUser(req, res) {
+
+  let { name, email, username, password } = req.body
+
+  if (!name || !email || !username || !password){
+    return res.status(400).json({error: 'All the fields are required'});
+  }
+
+  name = name.trim()
+  email = email.trim()
+  username = username.trim()
+
+  if (!/^[a-zA-Z0-9_-]{1,20}$/.test(username)){
+    return res.status(400).json({error: 'username must be 1-20 characters, using letters, numbers, _ or -.'})
+  }
+
+  if (!validator.isEmail(email)){
+    return res.status(400).json({error: 'Bad format of email'})
+  }
+
+  console.log(req.body)
+
+
+  try {
     
-    console.log('req.body', req.body)
+    const db = await getDBConnection()
+
+    let query = 'SELECT username FROM users WHERE username = ? OR email = ?' 
+    let params = [username, email]
+
+    const dbCheck = await db.all(query, params)
+
+    if (dbCheck.length){
+        return res.status(400).json({error: 'Username or an E-mail already in use'})
+    }
+
+    await db.exec('BEGIN TRANSACTION')
+
     
+    await db.run(`INSERT INTO users (name, email, username, password)
+        VALUES (?, ?, ?, ?)`,
+        [ name, email, username, password ]
+    )
+   
+
+    await db.exec('COMMIT')
+    res.status(201).json({message: `User ${username} registered`})
+
+  } catch (err) {
+    
+    console.error('Registration error:', err.message);
+    res.status(500).json({ error: 'Registration failed. Please try again.' })
+  }
+
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,25 +1,6 @@
-import validator from 'validator'
+import express from 'express'
+import { registerUser } from '../controllers/authController.js'
 
-export async function registerUser(req, res) {
+const authRouter = express.Router()
 
-  let { name, email, username, password } = req.body
-
-  if (!name || !email || !username || !password){
-    return res.status(400).json({error: 'All the fields are required'});
-  }
-
-  name = name.trim()
-  email = email.trim()
-  username = username.trim()
-
-  if (!/^[a-zA-Z0-9_-]{1,20}$/.test(username)){
-    return res.status(400).json({error: 'username must be 1-20 characters, using letters, numbers, _ or -.'})
-  }
-
-  if (!validator.isEmail(email)){
-    return res.status(400).json({error: 'Bad format of email'})
-  }
-
-  console.log(req.body)
-
-}
+authRouter.post('/register', registerUser)
