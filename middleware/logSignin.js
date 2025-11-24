2473663ccc8faa3e75dfbc015bae5465d0a10f5d
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-11-24 04:43:34 +0100

    feat(auth): remove logSignin middleware and add requireAuth middleware

--- middleware/logSignin.js
+++
@@ -1,7 +0,0 @@
-export function logSignin(req, res, next){
-    
-    console.log('sign in attempted')
-
-    next()
-
-}
---
+++ middleware/requireAuth.js
@@ -0,0 +1,3 @@
+export default function requireAuth(req, res, next){
+    
+}
