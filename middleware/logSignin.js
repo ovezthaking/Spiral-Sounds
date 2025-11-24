1aeaf73679dcdfe49d1ac396e761f05c2e69096b
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-11-24 04:41:21 +0100

    feat(auth): add logSignin middleware to log sign-in attempts

---
+++ middleware/logSignin.js
@@ -0,0 +1,7 @@
+export function logSignin(req, res, next){
+    
+    console.log('sign in attempted')
+
+    next()
+
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { loginUser, logoutUser, registerUser } from '../controllers/authController.js'
+import { logSignin } from '../middleware/logSignin.js'
 
 export const authRouter = express.Router()
 
 authRouter.post('/register', registerUser)
 
-authRouter.post('/login', loginUser)
+authRouter.post('/login', logSignin, loginUser)
 
 authRouter.get('/logout', logoutUser)
